4483888f148da4620e9b8cc91d5da59703c5c89d
David Dikman <ddikman@gmail.com>
2023-06-18 22:45:58 +0900

    First commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Refurbished MacBook Scraper
+
+Scraper to find all refurbished macbook pro's on sale in japan in order to try and find one that doesn't have a Japanese keyboard.
---
+++ scrape.js
@@ -0,0 +1,86 @@
+const { chromium } = require('playwright');
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const limit = 200;
+
+(async () => {
+  // Launch a new browser instance
+  const browser = await chromium.launch();
+
+  // Create a new browser context
+  const context = await browser.newContext();
+
+  // Create a new page
+  const page = await context.newPage();
+
+  // Navigate to the website
+  await page.goto('https://www.apple.com/jp/shop/refurbished/mac/macbook-pro');
+
+  const productPages = []
+
+  let listPage = 1;
+  while (true) {
+    console.log(`Parsing listing page ${listPage++}`)
+      // Grab all links
+    const linkHrefs = await page.$$eval('.rf-refurb-producttile-link', (links) =>
+      links.map((link) => link.href)
+    );
+    productPages.push(...linkHrefs)
+
+    // Go next page
+    const nextButton = await page.$('.rc-pagination .rc-pagination-arrow .icon-chevronright');
+    const nextButtonDisabled = await nextButton.getAttribute('aria-disabled')
+    if (nextButtonDisabled) {
+      break;
+    } else {
+      await nextButton.click();
+    }
+  }
+
+  let products = []
+
+  let productPageIndex = 1;
+  for (const productPage of productPages) {
+    console.log(`Parsing product page ${productPageIndex}/${productPages.length}`)
+    await page.goto(productPage)
+
+    const productOverviewInfo = await page.$$eval('.Overview-panel .para-list', (elements) =>
+      elements.map((element) => element.innerText)
+    )
+
+    const keyboard = await page.$$eval('.TechSpecs-panel .para-list', (elements) =>
+      elements.map((e) => e.innerText.trim()).filter((t) => t.includes('キーボード')).join(' ')
+    )
+
+    products.push({
+      year: productOverviewInfo[0],
+      size: productOverviewInfo[1].split('インチ')[0],
+      memory: productOverviewInfo[2],
+      disk: productOverviewInfo[3],
+      keyboard
+    })
+
+    productPageIndex++;
+    if (productPageIndex > limit) {
+      console.log(`hit limit of products (${limit}), aborting`)
+      break;
+    }
+  }
+
+  const csvWriter = createCsvWriter({
+    path: 'output.csv',
+    header: [
+      {id: 'year', title: 'Year'},
+      {id: 'size', title: 'Size'},
+      {id: 'memory', title: 'Memory'},
+      {id: 'disk', title: 'Disk'},
+      {id: 'keyboard', title: 'Keyboard'},
+    ]
+  });
+
+  await csvWriter.writeRecords(products)
+
+  // Close the browser
+  await browser.close();
+})();
